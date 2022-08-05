b225475d317527e7c72867456888db8855121cf5
Jeroen B <jeroenb04@live.com>
2022-08-05 14:55:18 +0200

    Add HTTP Response tests & other error handling tests

--- spec/route_xl_spec.rb
+++ spec/route_xl_spec.rb
@@ -4,7 +4,6 @@ require_relative '../lib/route_xl'
 
 describe RouteXL do
   let(:client) { RouteXL::RouteAPI.new('user', 'pass') }
-  let(:bad_client) { RouteXL::RouteAPI.new('baduser', 'badpass') }
 
   describe '#status' do
     context 'correct response with valid auth keys' do  
@@ -26,10 +25,59 @@ describe RouteXL do
     end
   end
 
+  describe 'HTTP Response & Other errors' do
+    let(:location) { instance_double("Location") }
+    
+
+    context 'Custom Error Testing' do
+      before(:each) do
+        allow(location).to receive(:instance_of?).with(RouteXL::Location).and_return(true)
+      end
+      
+      it 'raises ClassError when non-location is given' do
+        array = ['not_loca']
+        expect { client.distances(array) }.to raise_error(RouteXL::ClassError)
+      end
+
+      it 'raises SubscriptionError when 403 is returned' do
+        stub_request(:post, "https://api.routexl.com/distances").
+        with(
+          headers: {
+            'Authorization'=>'Basic dXNlcjpwYXNz',
+          'Connection'=>'close',
+          'Host'=>'api.routexl.com',
+          'User-Agent'=>'http.rb/5.1.0'
+          },
+          basic_auth: ['user', 'pass']
+        ).
+        to_return(status: 403, body: "", headers: {})
+
+        arr = Array.new(11, location)
+        expect { client.distances(arr) }.to raise_error(RouteXL::SubscriptionError)
+      end
+
+      it 'raises NoInputError when 409 is returned' do
+        stub_request(:post, "https://api.routexl.com/distances").
+        with(
+          headers: {
+            'Authorization'=>'Basic dXNlcjpwYXNz',
+          'Connection'=>'close',
+          'Host'=>'api.routexl.com',
+          'User-Agent'=>'http.rb/5.1.0'
+          },
+          basic_auth: ['user', 'pass']
+        ).
+        to_return(status: 409, body: "", headers: {})
+
+        arr = []
+        expect {client.distances(arr) }.to raise_error(RouteXL::NoInputError)
+      end
+    end
+  end
+
   describe '#distances' do
   end
 
   describe '#tour' do
   end
-
 end
